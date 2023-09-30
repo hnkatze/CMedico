ba29ef88e1c248fbda5d9a49fee5ffc01e32c800
Camilo Henriquez <henriquezhector1@hotmail.com>
2023-09-29 18:11:13 -0600

    change line for testing

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,6 +3,15 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
+
+  
+    // apiKey: "AIzaSyC8nxaIF8MwPBU2IUZReYcmPpsMCpFfuJE",
+    // authDomain: "testmedi-f012a.firebaseapp.com",
+    // projectId: "testmedi-f012a",
+    // storageBucket: "testmedi-f012a.appspot.com",
+    // messagingSenderId: "707651425260",
+    // appId: "1:707651425260:web:bcf12388d5e11ea61da6c9"
+  
   apiKey: "AIzaSyAMxWdvOFFkTrx3u4H-W7dwl7TlTy2YPGA",
   authDomain: "centromedico-4a9f1.firebaseapp.com",
   projectId: "centromedico-4a9f1",
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Navy from './Components/Navbar.jsx'
+//import AddConsulta from './Components/helpers/AddConsulta.jsx'
+//import Consulta from './Components/Consulta.jsx'
 import Inventory from './Components/Inventory.jsx'
+//import Consulta from './Components/Consulta.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Navy/>
     <Inventory />
+    {/* <Consulta /> */}
   </React.StrictMode>,
 )
