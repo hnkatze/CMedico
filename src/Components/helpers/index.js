cba56960641dea869841b01b10310c1adf7d149b
Camilo Henriquez <henriquezhector1@hotmail.com>
2023-10-07 13:56:01 -0600

    Created a directory of exports

---
+++ src/Components/helpers/index.js
@@ -0,0 +1,7 @@
+export { default as AddConsulta }  from "./AddConsulta";
+export { default as AddProduct } from "./AddProduct"; 
+export * from "./deleteConsulta";
+export * from "./deleteProduct";
+export * from "./updateProduct";
+export * from "firebase/firestore";
+export * from "antd"
