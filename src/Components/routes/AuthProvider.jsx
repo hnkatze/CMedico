244bff27f53ee28ef14c2f441f8e2c6fce7d421a
Camilo Henriquez <henriquezhector1@hotmail.com>
2023-10-16 22:01:51 -0600

    Created en use  useContext for simplify some props

---
+++ src/Components/routes/AuthProvider.jsx
@@ -0,0 +1,28 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [userType, setUserType] = useState(null);
+
+  const login = (type) => {
+    setIsAuthenticated(true);
+    setUserType(type);
+  };
+
+  const logout = () => {
+    setIsAuthenticated(false);
+    setUserType(null);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, userType, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => {
+  return useContext(AuthContext);
+};
